33aee0a8e9b56e76649adb74e267851d5e8b241f
Thomas Lecavelier <thomas@followanalytics.com>
2015-04-27 13:57:57 +0200

    Protection against missing record in AS

--- teleinfo_pusher.rb
+++ teleinfo_pusher.rb
@@ -11,7 +11,8 @@ class AerospikeConnector
 
   def add_hash(hash)
     @key = Aerospike::Key.new(@namespace, 'teleinfo', Time.now.strftime('%Y%m%d'))
-    record = @client.get(@key).bins || {}
+    record = @client.get(@key)
+    record = record && record.bins || {}
     record[Time.now.utc.to_i.to_s] = hash
     @client.put(@key, record)
   end
